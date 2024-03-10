34ee16c582127e8960d7d0850fe92f4f19d2487e
= <itzmohamed007@gmail.com>
2024-03-10 14:47:21 +0000

    init

---
+++ src/main/java/com/electify/ElectifyApplication.java
@@ -0,0 +1,13 @@
+package com.electify;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElectifyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ElectifyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/electify/ElectifyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.electify;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElectifyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
