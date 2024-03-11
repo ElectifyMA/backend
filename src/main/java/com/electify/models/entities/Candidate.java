ecbe0b0f99cc67dc1369c123cc4c9c2c30c73633
= <itzmohamed007@gmail.com>
2024-03-11 11:50:15 +0000

    Added all actors

---
+++ src/main/java/com/electify/models/entities/Candidate.java
@@ -0,0 +1,17 @@
+package com.electify.models.entities;
+
+import jakarta.persistence.Entity;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.SuperBuilder;
+
+@Getter
+@Setter
+@Entity
+@SuperBuilder
+@NoArgsConstructor
+public class Candidate extends User {
+
+}
