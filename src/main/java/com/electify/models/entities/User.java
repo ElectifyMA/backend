8b2defe366ea0bc9deed0f183c9de10bb6cff8a6
= <itzmohamed007@gmail.com>
2024-03-14 07:02:43 +0000

    Corrected inheritance strategy (from a single table to seperate tables)

--- src/main/java/com/electify/models/entities/User.java
+++ src/main/java/com/electify/models/entities/User.java
@@ -3,21 +3,21 @@ package com.electify.models.entities;
 import com.electify.models.enums.Role;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Inheritance;
+import jakarta.persistence.InheritanceType;
 import jakarta.persistence.Table;
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.experimental.SuperBuilder;
 
-import java.time.LocalDateTime;
+import java.time.LocalDate;
 
 @Getter
 @Setter
 @Entity
 @SuperBuilder
 @NoArgsConstructor
-@Inheritance
+@Inheritance(strategy = InheritanceType.JOINED)
 @Table(name = "_user")
 public class User extends AbstractEntity {
     private String firstName;
@@ -25,7 +25,7 @@ public class User extends AbstractEntity {
     private String email;
     private String password;
     private String cin;
-    private LocalDateTime birthDate;
+    private LocalDate birthDate;
     private String address;
     private boolean isActive;
     private Role role;
