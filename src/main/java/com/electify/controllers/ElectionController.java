d0bd6062d6239002a6ce140eec3d86c195b9f9ac
= <itzmohamed007@gmail.com>
2024-03-15 11:55:11 +0000

    Added Election controller

---
+++ src/main/java/com/electify/controllers/ElectionController.java
@@ -0,0 +1,21 @@
+package com.electify.controllers;
+
+import com.electify.models.dto.request.ElectionRequest;
+import com.electify.models.dto.response.ElectionResponse;
+import com.electify.services.impl.ElectionService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.UUID;
+
+@Slf4j
+@Validated
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("api/v1/elections")
+public class ElectionController extends _Controller<UUID, ElectionRequest, ElectionResponse, ElectionService>{
+
+}
