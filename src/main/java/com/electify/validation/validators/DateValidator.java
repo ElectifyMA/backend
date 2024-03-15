3f94d287204f04a3db0a481f13f631f5d71536ac
= <itzmohamed007@gmail.com>
2024-03-15 11:54:15 +0000

    Added Custom Date validation annotation

---
+++ src/main/java/com/electify/validation/validators/DateValidator.java
@@ -0,0 +1,25 @@
+package com.electify.validation.validators;
+
+import com.electify.validation.annotations.Date;
+import jakarta.validation.ConstraintValidator;
+import jakarta.validation.ConstraintValidatorContext;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+
+public class DateValidator implements ConstraintValidator<Date, String> {
+    private final DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+
+    @Override
+    public boolean isValid(String date, ConstraintValidatorContext context) {
+        if (date == null || date.isBlank()) return false;
+
+        try {
+            LocalDate.parse(date, dateFormatter);
+            return true;
+        } catch (DateTimeParseException e) {
+            return false;
+        }
+    }
+}
