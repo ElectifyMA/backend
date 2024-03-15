cf0598c0ebc99612e87ebed5dbe01684a7c98f9e
= <itzmohamed007@gmail.com>
2024-03-15 11:54:58 +0000

    Added Election service interface

---
+++ src/main/java/com/electify/services/spec/IElectionService.java
@@ -0,0 +1,10 @@
+package com.electify.services.spec;
+
+import com.electify.models.dto.request.ElectionRequest;
+import com.electify.models.dto.response.ElectionResponse;
+
+import java.util.UUID;
+
+public interface IElectionService extends _Service<UUID, ElectionRequest, ElectionResponse>{
+
+}
