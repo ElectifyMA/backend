083f93cc0463797fb15dc0c27c8bf516af258274
= <itzmohamed007@gmail.com>
2024-03-16 11:59:37 +0000

    Added abstract basic dto (dtos that do not contain any relations to solve jackson recursion errors)

---
+++ src/main/java/com/electify/models/dto/basic/AbstractBasic.java
@@ -0,0 +1,32 @@
+package com.electify.models.dto.basic;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.SuperBuilder;
+
+import java.sql.Timestamp;
+import java.util.UUID;
+
+@Getter
+@Setter
+@SuperBuilder
+@AllArgsConstructor
+@NoArgsConstructor
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class AbstractBasic {
+    /**
+     * The unique identifier of the entity.
+     */
+    private UUID id;
+    /**
+     * The timestamp when the entity was created.
+     */
+    private Timestamp createdAt;
+    /**
+     * The timestamp when the entity was last updated.
+     */
+    private Timestamp updatedAt;
+}
