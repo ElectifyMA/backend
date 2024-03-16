2b67ae0bf9a34ac7383c7f2bd32463a101fa31a1
= <itzmohamed007@gmail.com>
2024-03-16 11:57:05 +0000

    Added HttpStatus parameter to ModularException

--- src/main/java/com/electify/exceptions/customs/ModularException.java
+++ src/main/java/com/electify/exceptions/customs/ModularException.java
@@ -1,7 +1,13 @@
 package com.electify.exceptions.customs;
 
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@Getter
 public class ModularException extends RuntimeException {
-    public ModularException(String message) {
+    private final HttpStatus status;
+    public ModularException(String message, HttpStatus httpStatus) {
         super(message);
+        status = httpStatus;
     }
 }
