48e718a204fa914b38e7ae5d5ac92b8ce27e040f
= <itzmohamed007@gmail.com>
2024-03-16 12:02:45 +0000

    optimised imports in election request dto

--- src/main/java/com/electify/models/dto/request/ElectionRequest.java
+++ src/main/java/com/electify/models/dto/request/ElectionRequest.java
@@ -1,23 +1,15 @@
 package com.electify.models.dto.request;
 
-import com.electify.models.entities.Constituency;
-import com.electify.models.entities.NationalList;
-import com.electify.models.entities.PoliticalParty;
 import com.electify.models.enums.ElectionStatus;
 import com.electify.models.enums.ElectionType;
 import com.electify.validation.annotations.Date;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import jakarta.persistence.*;
-import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.experimental.SuperBuilder;
-
-import java.time.LocalDate;
-import java.util.List;
 
 @Getter
 @Setter
