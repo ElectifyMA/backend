598f9e789e1a2bfbe43a37863da8047551a852ca
= <itzmohamed007@gmail.com>
2024-03-16 12:01:04 +0000

    added constituency controller

---
+++ src/main/java/com/electify/controllers/ConstituencyController.java
@@ -0,0 +1,21 @@
+package com.electify.controllers;
+
+import com.electify.models.dto.request.ConstituencyRequest;
+import com.electify.models.dto.response.ConstituencyResponse;
+import com.electify.services.impl.ConstituencyService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.UUID;
+
+@Slf4j
+@Validated
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("api/v1/constituency")
+public class ConstituencyController extends _Controller<UUID, ConstituencyRequest, ConstituencyResponse, ConstituencyService> {
+
+}
