648ac3a66dcc78f467e51c96fa007c8bf2c20052
= <itzmohamed007@gmail.com>
2024-03-17 12:30:33 +0000

    Added implementation definition in City service

--- src/main/java/com/electify/services/impl/CityService.java
+++ src/main/java/com/electify/services/impl/CityService.java
@@ -5,11 +5,12 @@ import com.electify.models.dto.request.CityRequest;
 import com.electify.models.dto.response.CityResponse;
 import com.electify.models.entities.City;
 import com.electify.repositories.CityRepository;
+import com.electify.services.spec.ICityService;
 import org.springframework.stereotype.Service;
 
 import java.util.UUID;
 
 @Service
-public class CityService extends _AbstractService<UUID, CityRequest, CityResponse, City, CityRepository, CityMapper> {
+public class CityService extends _AbstractService<UUID, CityRequest, CityResponse, City, CityRepository, CityMapper> implements ICityService {
 
 }
