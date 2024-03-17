51cd629bbeb340ca45db05fbdb0d613fd35d5233
= <itzmohamed007@gmail.com>
2024-03-17 12:27:37 +0000

    Added implementation definition in Region service

--- src/main/java/com/electify/services/impl/RegionService.java
+++ src/main/java/com/electify/services/impl/RegionService.java
@@ -5,11 +5,12 @@ import com.electify.models.dto.request.RegionRequest;
 import com.electify.models.dto.response.RegionResponse;
 import com.electify.models.entities.Region;
 import com.electify.repositories.RegionRepository;
+import com.electify.services.spec.IRegionService;
 import org.springframework.stereotype.Service;
 
 import java.util.UUID;
 
 @Service
-public class RegionService extends _AbstractService<UUID, RegionRequest, RegionResponse, Region, RegionRepository, RegionMapper> {
+public class RegionService extends _AbstractService<UUID, RegionRequest, RegionResponse, Region, RegionRepository, RegionMapper> implements IRegionService {
 
 }
