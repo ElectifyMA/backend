139b8bf6301e5c21ffccc087c0defc6950d11441
= <itzmohamed007@gmail.com>
2024-03-17 10:44:35 +0000

    Optimized imports in AbstractRequest

--- src/main/java/com/electify/models/dto/request/AbstractRequest.java
+++ src/main/java/com/electify/models/dto/request/AbstractRequest.java
@@ -1,7 +1,6 @@
 package com.electify.models.dto.request;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
