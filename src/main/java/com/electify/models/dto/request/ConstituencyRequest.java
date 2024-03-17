8df765fe3d4effbb3acacfe061fb56e073f35d89
= <itzmohamed007@gmail.com>
2024-03-17 11:10:48 +0000

    Improved validation in ConstituencyRequest dto

--- src/main/java/com/electify/models/dto/request/ConstituencyRequest.java
+++ src/main/java/com/electify/models/dto/request/ConstituencyRequest.java
@@ -4,6 +4,7 @@ import com.electify.models.dto.response.ElectionResponse;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -19,6 +20,7 @@ import lombok.experimental.SuperBuilder;
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class ConstituencyRequest extends AbstractRequest {
     @NotNull(message = "name cannot be null")
+    @NotEmpty(message = "name cannot be empty")
     private String name;
     @NotNull(message = "chairs count cannot be null")
     @Min(value = 1, message = "chairs count cannot be less than 1 chair")
