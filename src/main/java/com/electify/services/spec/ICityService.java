19410741bc5c5780e19ab53872ae92e9e5404d23
= <itzmohamed007@gmail.com>
2024-03-17 11:52:06 +0000

    Added City service interface

---
+++ src/main/java/com/electify/services/spec/ICityService.java
@@ -0,0 +1,10 @@
+package com.electify.services.spec;
+
+import com.electify.models.dto.request.CityRequest;
+import com.electify.models.dto.response.CityResponse;
+
+import java.util.UUID;
+
+public interface ICityService extends _Service<UUID, CityRequest, CityResponse> {
+
+}
