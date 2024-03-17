910e71fd465c192971812ee584424ef06653f21e
= <itzmohamed007@gmail.com>
2024-03-17 11:46:28 +0000

    Added City basic dto

---
+++ src/main/java/com/electify/models/dto/basic/CityBasic.java
@@ -0,0 +1,18 @@
+package com.electify.models.dto.basic;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.SuperBuilder;
+
+@Getter
+@Setter
+@SuperBuilder
+@AllArgsConstructor
+@NoArgsConstructor
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class CityBasic extends AbstractBasic {
+    private String name;
+}
