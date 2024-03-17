021c743fbd1f71e9a2511d8d86f4142a3c58dcf0
= <itzmohamed007@gmail.com>
2024-03-17 10:45:05 +0000

    Optimized imports in ConstituencyRequest

--- src/main/java/com/electify/models/dto/request/ConstituencyRequest.java
+++ src/main/java/com/electify/models/dto/request/ConstituencyRequest.java
@@ -1,26 +1,15 @@
 package com.electify.models.dto.request;
 
 import com.electify.models.dto.response.ElectionResponse;
-import com.electify.models.entities.Candidacy;
-import com.electify.models.entities.City;
-import com.electify.models.entities.Election;
-import com.electify.models.entities.PartyBranch;
-import com.electify.models.enums.ElectionStatus;
-import com.electify.models.enums.ElectionType;
-import com.electify.validation.annotations.Date;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import jakarta.persistence.ManyToOne;
-import jakarta.persistence.OneToMany;
 import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.experimental.SuperBuilder;
-import org.mapstruct.Mapper;
 
 @Getter
 @Setter
