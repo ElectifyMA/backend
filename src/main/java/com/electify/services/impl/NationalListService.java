c8cca0b79474548b6680cd36303185deee60fa2a
= <itzmohamed007@gmail.com>
2024-03-17 12:28:01 +0000

    Added implementation definition in NationalList service

--- src/main/java/com/electify/services/impl/NationalListService.java
+++ src/main/java/com/electify/services/impl/NationalListService.java
@@ -5,11 +5,12 @@ import com.electify.models.dto.request.NationalListRequest;
 import com.electify.models.dto.response.NationalListResponse;
 import com.electify.models.entities.NationalList;
 import com.electify.repositories.NationalListRepository;
+import com.electify.services.spec.INationalListService;
 import org.springframework.stereotype.Service;
 
 import java.util.UUID;
 
 @Service
-public class NationalListService extends _AbstractService<UUID, NationalListRequest, NationalListResponse, NationalList, NationalListRepository, NationalListMapper> {
+public class NationalListService extends _AbstractService<UUID, NationalListRequest, NationalListResponse, NationalList, NationalListRepository, NationalListMapper> implements INationalListService {
 
 }
