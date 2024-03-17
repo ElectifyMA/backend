97d6d4696b3461e6ffdd95d8e8fe78c08c1fa693
= <itzmohamed007@gmail.com>
2024-03-17 10:48:10 +0000

    Added NationalList request dto

---
+++ src/main/java/com/electify/models/dto/request/NationalListRequest.java
@@ -0,0 +1,25 @@
+package com.electify.models.dto.request;
+
+import com.electify.models.dto.response.ElectionResponse;
+import com.electify.validation.annotations.Date;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.SuperBuilder;
+
+@Getter
+@Setter
+@SuperBuilder
+@AllArgsConstructor
+@NoArgsConstructor
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class NationalListRequest extends AbstractRequest {
+    @NotNull(message = "date cannot be null")
+    @Date
+    private String date;
+    @NotNull(message = "election cannot be null")
+    private ElectionResponse election;
+}
