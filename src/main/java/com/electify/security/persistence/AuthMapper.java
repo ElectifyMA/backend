eb4c96e5adad9c4ebcdca8c97c5e3079d54ebe17
= <itzmohamed007@gmail.com>
2024-03-19 13:45:38 +0000

    Added Auth mapper to map between authentication objects

---
+++ src/main/java/com/electify/security/persistence/AuthMapper.java
@@ -0,0 +1,24 @@
+package com.electify.security.persistence;
+
+
+import com.electify.models.dto.auth.AuthUser;
+import com.electify.models.dto.auth.Registration;
+import com.electify.models.dto.response.UserResponse;
+import com.electify.models.entities.User;
+import org.mapstruct.InjectionStrategy;
+import org.mapstruct.Mapper;
+import org.mapstruct.MappingConstants;
+import org.mapstruct.ReportingPolicy;
+import org.mapstruct.control.MappingControl;
+import org.springframework.security.core.userdetails.UserDetails;
+
+@Mapper(
+        unmappedTargetPolicy = ReportingPolicy.WARN,
+        injectionStrategy = InjectionStrategy.CONSTRUCTOR,
+        componentModel = MappingConstants.ComponentModel.SPRING
+)
+public interface AuthMapper {
+    UserResponse toResponse(User user);
+    User toEntity(Registration registration);
+    AuthUser toUserDetails(User user);
+}
