d4ce604b7dd92ff4670d5dc32db5c4dedf8dbd51
= <itzmohamed007@gmail.com>
2024-03-19 12:27:30 +0000

    Added unique constraint in User entity

--- src/main/java/com/electify/models/entities/User.java
+++ src/main/java/com/electify/models/entities/User.java
@@ -1,14 +1,12 @@
 package com.electify.models.entities;
 
 import com.electify.models.enums.Role;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Inheritance;
-import jakarta.persistence.InheritanceType;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.experimental.SuperBuilder;
+import org.hibernate.validator.constraints.UniqueElements;
 
 import java.time.LocalDate;
 
@@ -22,11 +20,14 @@ import java.time.LocalDate;
 public class User extends AbstractEntity {
     private String firstName;
     private String lastName;
+    @Column(unique = true)
     private String email;
     private String password;
+    @Column(unique = true)
     private String cin;
     private LocalDate birthDate;
     private String address;
+    @Column(columnDefinition = "BOOLEAN DEFAULT true")
     private boolean isActive;
     private Role role;
 }
