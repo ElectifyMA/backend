af7a3fc38e065452e63c276ce1f1b3efe1339579
= <itzmohamed007@gmail.com>
2024-03-19 12:27:42 +0000

    Added registration dto

---
+++ src/main/java/com/electify/models/dto/auth/Registration.java
@@ -0,0 +1,33 @@
+package com.electify.models.dto.auth;
+
+import com.electify.models.enums.Role;
+import com.electify.validation.annotations.Cin;
+import com.electify.validation.annotations.Date;
+import jakarta.validation.constraints.*;
+import lombok.*;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Registration {
+    @NotBlank(message = "First name is required")
+    private String firstName;
+    @NotBlank(message = "Last name is required")
+    private String lastName;
+    @Email(message = "Invalid email format (Ex: example@gmail.com)")
+    @NotBlank(message = "Email address is required")
+    private String email;
+    @NotBlank(message = "Password is required")
+    private String password;
+    @Cin(message = "Cin code should respect this format (Ex: XX999999)")
+    @NotBlank(message = "Cin code is required")
+    private String cin;
+    @Date
+    private String birthDate;
+    @NotBlank(message = "Address is required")
+    private String address;
+    @NotNull(message = "Role is required")
+    private Role role;
+}
