a2526f5259b888560cb38114b66b8d1e6e087355
= <itzmohamed007@gmail.com>
2024-03-20 15:15:36 +0000

    Added SuperAdmin mapper

---
+++ src/main/java/com/electify/mappers/auth/SuperAdminMapper.java
@@ -0,0 +1,18 @@
+package com.electify.mappers.auth;
+
+import com.electify.models.entities.SuperAdmin;
+import org.mapstruct.InjectionStrategy;
+import org.mapstruct.Mapper;
+import org.mapstruct.MappingConstants;
+import org.mapstruct.ReportingPolicy;
+
+import java.util.UUID;
+
+@Mapper(
+        unmappedTargetPolicy = ReportingPolicy.WARN,
+        injectionStrategy = InjectionStrategy.CONSTRUCTOR,
+        componentModel = MappingConstants.ComponentModel.SPRING
+)
+public interface SuperAdminMapper extends AuthMapper<UUID, SuperAdmin> {
+
+}
