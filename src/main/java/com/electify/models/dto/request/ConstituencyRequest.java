4031365c6a5fb8332a3ba133581b89cfa5809330
= <itzmohamed007@gmail.com>
2024-03-20 15:12:22 +0000

    Optimized imports

--- src/main/java/com/electify/models/dto/request/ConstituencyRequest.java
+++ src/main/java/com/electify/models/dto/request/ConstituencyRequest.java
@@ -2,7 +2,10 @@ package com.electify.models.dto.request;
 
 import com.electify.models.dto.response.ElectionResponse;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import jakarta.validation.constraints.*;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
