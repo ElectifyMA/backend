c2dd23fef5f1ed7cc664d7e7af63cad9290c2f6b
= <itzmohamed007@gmail.com>
2024-03-28 12:33:33 +0000

    added party branch service interface

---
+++ src/main/java/com/electify/services/spec/IPartyBranchService.java
@@ -0,0 +1,10 @@
+package com.electify.services.spec;
+
+import com.electify.models.dto.request.PartyBranchRequest;
+import com.electify.models.dto.response.PartyBranchResponse;
+
+import java.util.UUID;
+
+public interface IPartyBranchService extends _Service<UUID, PartyBranchRequest, PartyBranchResponse> {
+
+}
