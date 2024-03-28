2348c190885b8cf095a928907c7b13532bf0371a
= <itzmohamed007@gmail.com>
2024-03-28 12:33:48 +0000

    added party branch controller

---
+++ src/main/java/com/electify/controllers/PartyBranchController.java
@@ -0,0 +1,21 @@
+package com.electify.controllers;
+
+import com.electify.models.dto.request.PartyBranchRequest;
+import com.electify.models.dto.response.PartyBranchResponse;
+import com.electify.services.impl.PartyBranchService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.UUID;
+
+@Slf4j
+@Validated
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("api/v1/party-branch")
+public class PartyBranchController extends _Controller<UUID, PartyBranchRequest, PartyBranchResponse, PartyBranchService> {
+
+}
