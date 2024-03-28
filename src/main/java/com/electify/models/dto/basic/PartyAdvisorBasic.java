39024df20726c03c92378860b75c0e826a44d5f1
= <itzmohamed007@gmail.com>
2024-03-28 12:25:36 +0000

    added party advisor basic dto

---
+++ src/main/java/com/electify/models/dto/basic/PartyAdvisorBasic.java
@@ -0,0 +1,16 @@
+package com.electify.models.dto.basic;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.experimental.SuperBuilder;
+
+@Getter
+@Setter
+@SuperBuilder
+@AllArgsConstructor
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class PartyAdvisorBasic extends UserBasic {
+
+}
