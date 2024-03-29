d42c39f25c911627e602e56fb45bdb6830a27305
= <itzmohamed007@gmail.com>
2024-03-29 14:33:48 +0000

    added vote controller

---
+++ src/main/java/com/electify/controllers/VoteController.java
@@ -0,0 +1,21 @@
+package com.electify.controllers;
+
+import com.electify.models.dto.request.VoteRequest;
+import com.electify.models.dto.response.VoteResponse;
+import com.electify.services.impl.VoteService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.UUID;
+
+@Slf4j
+@Validated
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("api/${version}/vote")
+public class VoteController extends _Controller<UUID, VoteRequest, VoteResponse, VoteService> {
+
+}
