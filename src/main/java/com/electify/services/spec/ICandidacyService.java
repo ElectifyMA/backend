bc9dbce085d3edf9ad2454e0976cc37bc08e93fd
= <itzmohamed007@gmail.com>
2024-03-29 06:36:00 +0000

    added candidacy service interface

---
+++ src/main/java/com/electify/services/spec/ICandidacyService.java
@@ -0,0 +1,10 @@
+package com.electify.services.spec;
+
+import com.electify.models.dto.request.CandidacyRequest;
+import com.electify.models.dto.response.CandidacyResponse;
+
+import java.util.UUID;
+
+public interface ICandidacyService extends _Service<UUID, CandidacyRequest, CandidacyResponse>{
+
+}
