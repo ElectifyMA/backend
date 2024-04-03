74707a4fbd8f5cc96c1ff1fa44baf605bdc40fdc
= <itzmohamed007@gmail.com>
2024-04-03 10:15:34 +0000

    added candidate related dto

---
+++ src/main/java/com/electify/models/dto/related/CandidateRelated.java
@@ -0,0 +1,30 @@
+package com.electify.models.dto.related;
+
+import com.electify.models.enums.Role;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.SuperBuilder;
+
+import java.time.LocalDate;
+
+@Getter
+@Setter
+@SuperBuilder
+@AllArgsConstructor
+@NoArgsConstructor
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class CandidateRelated {
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String password;
+    private String cin;
+    private LocalDate birthDate;
+    private String address;
+    private boolean isActive;
+    private Role role;
+    private PartyBranchRelated partyBranch;
+}
