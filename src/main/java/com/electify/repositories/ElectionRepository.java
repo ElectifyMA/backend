5254cd4ac8ff2a392f28fa054369e51fe728171c
= <itzmohamed007@gmail.com>
2024-04-03 10:13:51 +0000

    added getting latest election method in election repository

--- src/main/java/com/electify/repositories/ElectionRepository.java
+++ src/main/java/com/electify/repositories/ElectionRepository.java
@@ -1,12 +1,14 @@
 package com.electify.repositories;
 
 import com.electify.models.entities.Election;
+import com.electify.models.enums.ElectionStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface ElectionRepository extends JpaRepository<Election, UUID> {
-
+    Optional<Election> findTopByStatusOrderByCreatedAtDesc(ElectionStatus status);
 }
